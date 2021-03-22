c33a006f53634537933ab26baae5e03390314faa
roshnirsks <kumariroshni81998@gmail.com>
2021-03-22 16:35:50 +0530

    function hrader add

--- math.py
+++ math.py
@@ -1,17 +1,15 @@
+#add imp.
 def add(x,y):
-    return x+y
-#gh
-def subtract(x,y):
-
-    retiutn x-y
-
-
-#ghhv
-def multiply(x,y):
-    return x*y
-def divide(x,y):
     pass
 
-#sq imp
-def sq(x):
-	pass
+#Sub imp
+def subtract(x,y):
+    pass
+
+#Mulimp
+def multiply(x,y):
+    pass
+
+#Div imp
+def divide(x,y):
+    pass
